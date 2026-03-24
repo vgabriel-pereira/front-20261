96712c56f1df2b6679e305b7892b30d7b400417b
Vitor <gabrielptrindade02@gmail.com>
2026-03-24 20:29:34 -0300

    feat: conclui pratica02. Fecha #2

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,33 @@
-import './style.css'
+import './style.css';
+
+document.getElementById("formularioLogin").addEventListener('submit',function(e){
+    e.preventDefault();
+
+    const EmailInput = document.querySelector('#email');
+    const senhaInput = document.querySelector('#senha');
+    const erroEmail = document.querySelector('#emailErro');
+    const erroSenha = document.querySelector('#senhaErro');
+
+    erroEmail.textContent = '';
+    erroSenha.textContent = '';
+ 
+    //O formulário não pode ser enviado se houver campos vazios
+    if(EmailInput.value === '' && senhaInput.value === ''){
+        erroEmail.textContent = "O Campo de email é obrigatório";
+        erroSenha.textContent = "O Campo de senha é obrigatório";
+        return;
+    }
+
+
+    if(EmailInput.value === '') {
+        erroEmail.textContent = "O Campo de email é obrigatório";
+        return;
+    }
+
+    if(senhaInput.value === '') {
+        erroSenha.textContent = "O Campo de senha é obrigatório";
+        return;
+    }
+
+    window.location.href = "http://localhost:5173/";
+})
